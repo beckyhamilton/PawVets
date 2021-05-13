c5b5039e6645cdbd21d297a130d1a28321843ebe
Becky Hamilton <hamilton-r32@ulster.ac.uk>
2021-05-13 18:31:55 +0100

    application finalised - everything working

--- app/src/main/java/com/example/PawVets/Appointment.java
+++ app/src/main/java/com/example/PawVets/Appointment.java
@@ -20,6 +20,7 @@ import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 import com.example.PawVets.Model.Appt;
+import com.example.PawVets.Model.Booking;
 import com.example.PawVets.Model.User;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -31,6 +32,8 @@ import com.shuhart.stepview.StepView;
 
 import org.w3c.dom.Text;
 
+import java.sql.Array;
+import java.sql.Time;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -42,9 +45,23 @@ import butterknife.ButterKnife;
 
 public class Appointment extends AppCompatActivity {
 
+    private TextView waitTime;
+    private ArrayList<TextView> times;
+    private Button date, time, book;
+    private MaterialSpinner serviceSpinner;
+    private ArrayList<Booking> bookings;
 
-    final FirebaseDatabase database = FirebaseDatabase.getInstance();
-    final DatabaseReference dbref = database.getReference("AvailableAppointments");
+    private static DatabaseReference databaseBookings = FirebaseDatabase.getInstance().getReference("Bookings");
+    private static DatabaseReference databaseServices = FirebaseDatabase.getInstance().getReference("Services");
+
+    private DatePicker aptDate;
+    private TimePicker aptTime;
+
+    private Date currentDate = new Date(System.currentTimeMillis());
+    private Date savedDate;
+    private boolean pickedTime;
+
+  //  String[] services = {"Select Appointment Type", "Vet Consultation", "First Vaccination", "Health Check", "Other"};
 
 
     @Override
@@ -53,18 +70,20 @@ public class Appointment extends AppCompatActivity {
         setContentView(R.layout.activity_appointment);
 
 
+        //Adding drop down options for spinner
+        final MaterialSpinner spinner = findViewById(R.id.selectservice_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.services, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+     //   serviceSpinner.setAdapter(adapter);
+
+        }
+            }
 
 
 
 
 
+                ///Set up selecting pet to book appointment for
+                /// Create list for reason for appointment
+                /// set current date into textview - use when viewing appointment
 
-
-        ///Set up selecting pet to book appointment for
-        /// Create list for reason for appointment
-    }
-
-
-
-
-}
